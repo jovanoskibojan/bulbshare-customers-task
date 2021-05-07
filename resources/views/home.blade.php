790852a54ae09dd5a67bc80bcfc8b7e2e91659a9
Bojan <kontakt@jovanoskibojan.com>
2021-05-07 08:39:31 +0200

    Homepage added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bulbshare TechTask - fullstack</title>
+    <!-- DataTables CSS -->
+    <link id="theme-style" rel="stylesheet" href="cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+</head>
+<body>
+<!-- DataTables JS -->
+<script type="text/javascript" src="cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+</body>
+</html>
